ad92dd2ea014db202728694c05db5e8a10762045
DhaneshPachipulusu <pdhaneswararao@gmail.com>
2026-01-24 11:08:37 +0530

    convert ai-frontend from submodule to normal folder12

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ app = FastAPI(title="AI Interview Bot")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000","https://ai-bot-ikyi.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
