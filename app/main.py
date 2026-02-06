c57cb23624a7122dd50bb791930a03f1be45624a
DhaneshPachipulusu <pdhaneswararao@gmail.com>
2026-02-06 23:43:26 +0530

    hi

--- app/main.py
+++ app/main.py
@@ -24,7 +24,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "https://ai-bot-ewl3.vercel.app"
+        "https://ai-bot-nlto.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
